0dca701da02e6de5620db5ab87c5175ecc18cb5b
MalenaMoix <moixmalena@gmail.com>
2018-04-10 11:29:14 -0300

    version inicial

---
+++ src/died/guias/guia02/app/Hola2.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package died.guias.guia02.app;
+
+/**
+ * @author Malena Moix
+ *
+ */
+public class Hola2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("el tiempo actual en milis es "+
+				System.currentTimeMillis());
+
+	}
+
+}
